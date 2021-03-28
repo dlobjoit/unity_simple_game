d0e47318feb9c5ee7a2ded7619582e0d8226fe05
dorian <dorian.lobjoit@orange.fr>
2021-03-28 13:20:50 +0200

    Ajout de mouvements latéraux sur le joueur basés sur des inputs clavier

--- game/Assets/PlayerMovement.cs
+++ game/Assets/PlayerMovement.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public Rigidbody rb;
-	public float inputForce;
+	public float forwardForce;
+	public float sidewayForce;
 
     // // Start is called before the first frame update
     // void Start()
@@ -15,6 +16,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, inputForce);
+        rb.AddForce(0, 0, forwardForce);
+        if(Input.GetKey("d"))
+        {
+        	rb.AddForce(sidewayForce, 0, 0);
+        }
+        else if(Input.GetKey("q"))
+        {
+        	rb.AddForce(-sidewayForce, 0, 0);
+        }
     }
 }
