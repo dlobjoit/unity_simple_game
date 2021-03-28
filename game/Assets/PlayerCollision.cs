23b6d2d23da20191e939708766aaa6bd5c2b7e9a
dorian <dorian.lobjoit@orange.fr>
2021-03-28 14:25:17 +0200

    Ajout d'obstacles supplémentaires, modifications de matériaux

--- game/Assets/PlayerCollision.cs
+++ game/Assets/PlayerCollision.cs
@@ -5,7 +5,7 @@ public class PlayerCollision : MonoBehaviour
 	public PlayerMovement movement;
 	void OnCollisionEnter(Collision collisionInfo)
 	{
-		if(collisionInfo.collider.tag == "Obstacle")
+		if(collisionInfo.collider.tag == "obstacle")
 		{
 			movement.enabled = false;
 		}
