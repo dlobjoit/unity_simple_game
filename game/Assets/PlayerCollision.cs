b046aa16dcf7311cc280a3e30de310e37650feae
dorian <dorian.lobjoit@orange.fr>
2021-03-28 14:04:48 +0200

    Modification du script de collision

--- game/Assets/PlayerCollision.cs
+++ game/Assets/PlayerCollision.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+	public PlayerMovement movement;
 	void OnCollisionEnter(Collision collisionInfo)
 	{
-		Debug.Log(collisionInfo.collider.name);
+		if(collisionInfo.collider.tag == "Obstacle")
+		{
+			movement.enabled = false;
+		}
 	}
 
 }
